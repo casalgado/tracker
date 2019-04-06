746e8f7617fae3cde25504e7f83795baa376e857
casalgado <casalgado86@yahoo.com>
2019-04-06 15:37:07 -0500

    converts text to date

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ document.getElementById('entryInputForm').addEventListener('submit', saveEntry);
 
 function saveEntry(e) {
   var entryId = 1;
-  var entryStart = document.getElementById('entryStart').value;
-  var entryEnd = document.getElementById('entryEnd').value;
+  var entryStart = convertToDate(document.getElementById('entryStart').value);
+  var entryEnd = convertToDate(document.getElementById('entryEnd').value);
 
   var entry = {
     id: entryId,
@@ -50,6 +50,12 @@ function saveEntry(e) {
   focusStart()
   e.preventDefault();
 
+}
+
+function convertToDate(num) {
+	d = new Date()
+	entryDate = new Date(d.getFullYear(), d.getMonth(), d.getDate(), parseInt(num.substring(0,2)), parseInt(num.substring(2,4)))
+	return entryDate
 }
 
 var moveFocus = function moveFocus(num) {
