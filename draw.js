9ffe0d867195eef5225a9e5c0a47a849a0ce086d
casalgado <casalgado86@yahoo.com>
2019-04-13 12:54:25 -0500

    new styles

--- draw.js
+++ draw.js
@@ -3,19 +3,34 @@ function drawEntry(entry) {									// draws each entry
 	var list = document.getElementById('entryList')			// assigns DOM element entryList to variable 
 
 
-	if (document.getElementById(entryYMD(entry))) {			// determines if an entryContainer has been drawn for an entry
-		var cont = document.getElementById(entryYMD(entry)) // with the same date. If so, assigns it to a variable.
+	if (document.getElementById('ec-' + entryYMD(entry))) {			       // determines if an entryContainer has been drawn for an entry
+		var cont       = document.getElementById('ec-' + entryYMD(entry))  // with the same date. If so, assigns it to a variable.
+		var visualCont = document.getElementById('vc-' + entryYMD(entry))  // Also assigns visual container and date container to variable.
+		var dateCont   = document.getElementById('dc-' + entryYMD(entry))
 	} else {
 		var cont = document.createElement('div')			// if no entryContainer exists for that day, draws it. 
 		cont.className = 'entryContainer'					// and appends it to entryList
-		cont.id = entryYMD(entry)
+		cont.id = 'ec-' + entryYMD(entry)
 		list.appendChild(cont)
+
+		var visualCont = document.createElement('div')		// creates a visualization container to hold the
+		visualCont.className = 'visualContainer' 			// entry visualization
+		visualCont.id = 'vc-' + entryYMD(entry)
+		cont.appendChild(visualCont)
+
+		var dateCont = document.createElement('p')		// creates a date container to hold date data for entry
+		dateCont.className = 'dateContainer'
+		dateCont.id = 'dc-' + entryYMD(entry)
+		dateCont.innerHTML = moment(entry.start).format('dd MMM D')
+		cont.prepend(dateCont)
 
 		for (var i = 0; i < 24; i++) {						// creates segments used to mark the time of day
 	    	var segment = document.createElement('div')	 	// appends them to entryContainer
 	    	segment.className = 'hourSegment'
-	    	cont.appendChild(segment)
+	    	visualCont.appendChild(segment)
 	  	}
+
+
 	}
 															// creates entry bar, used to visualize time and duration of entry
   	var entryBar = document.createElement('div')            // creates DOM elemens
@@ -26,7 +41,7 @@ function drawEntry(entry) {									// draws each entry
   	entryBar.style.left = entryStart + '%'					// start
   	entryBar.style.width = entryEnd - entryStart + '%'	    // duration
 
-  	cont.appendChild(entryBar)								// appends entryBar to entry Container
+  	visualCont.appendChild(entryBar)								// appends entryBar to entry Container
 }
 
 function drawLegend() {										// draws legend to be used atop entry list
